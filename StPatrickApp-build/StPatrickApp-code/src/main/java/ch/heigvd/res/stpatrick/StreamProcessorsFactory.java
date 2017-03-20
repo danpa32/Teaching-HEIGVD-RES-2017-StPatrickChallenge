b5a76629df852b3aaa83892151201aaaed4db5a4
Daniel Palumbo <dpalumbo06@gmail.com>
2017-03-20 11:46:40 +0100

    not working yet

--- StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
+++ StPatrickApp-build/StPatrickApp-code/src/main/java/ch/heigvd/res/stpatrick/StreamProcessorsFactory.java
@@ -1,5 +1,8 @@
 package ch.heigvd.res.stpatrick;
 
+import java.io.*;
+import java.nio.Buffer;
+
 /**
  * This class is responsible for providing different types of Stream Processors.
  * 
@@ -14,6 +17,16 @@ public class StreamProcessorsFactory implements IStreamProcessorsFactory {
 
   @Override
   public IStreamProcessor getProcessor(String processorName) throws UnknownNameException {
+    if(processorName == "e-remover"){
+      return new IStreamProcessor() {
+        @Override
+        public void process(Reader in, Writer out) throws IOException {
+          BufferedReader rn = new BufferedReader(in);
+          BufferedWriter bw = new BufferedWriter(out);
+
+        }
+      };
+    }
     throw new UnknownNameException("The factory does not know any processor called " + processorName);
   }
 
